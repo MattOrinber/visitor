52c63ff36c92291132818f9d17747fb987b8afde
wu meng <mengw@oupeng.com>
2014-11-29 15:43:53 +0800

    delete the pinyin jar
    Signed-off-by: wu meng <mengw@oupeng.com>

--- visitor-front/src/main/java/org/visitor/appportal/web/utils/PinyinUtil.java
+++ visitor-front/src/main/java/org/visitor/appportal/web/utils/PinyinUtil.java
@@ -6,11 +6,6 @@ import org.springframework.util.StringUtils;
 
 import org.visitor.util.AppStringUtils;
 
-import net.sourceforge.pinyin4j.PinyinHelper;
-import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
-import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
-import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
-import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 
 public class PinyinUtil {
 	/**
@@ -20,27 +15,8 @@ public class PinyinUtil {
 	 * @return 汉语拼音首字母
 	 */
 	public static String cn2FirstSpell(String chinese) {
-		StringBuffer pybf = new StringBuffer();
-		char[] arr = chinese.toCharArray();
-		HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat();
-		defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE);
-		defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] > 128) {
-				try {
-					String[] _t = PinyinHelper.toHanyuPinyinStringArray(arr[i],
-							defaultFormat);
-					if (_t != null) {
-						pybf.append(_t[0].charAt(0));
-					}
-				} catch (BadHanyuPinyinOutputFormatCombination e) {
-					e.printStackTrace();
-				}
-			} else {
-				pybf.append(arr[i]);
-			}
-		}
-		return pybf.toString().replaceAll("\\W", "").trim();
+		
+		return "";
 	}
 
 	/**
@@ -51,33 +27,10 @@ public class PinyinUtil {
 	 * @return 汉语拼音
 	 */
 	public static String cn2Spell(String chinese) {
-		StringBuffer pybf = new StringBuffer();
-		char[] arr = chinese.toCharArray();
-		HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat();
-		defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE);
-		defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] > 128) {
-				try {
-					String[] _t = PinyinHelper.toHanyuPinyinStringArray(arr[i], defaultFormat);
-					if (_t != null && _t.length > 0) {
-						pybf.append(_t[0]);
-					}
-				} catch (BadHanyuPinyinOutputFormatCombination e) {
-					e.printStackTrace();
-				}
-			} else {
-				pybf.append(arr[i]);
-			}
-		}
-		return StringUtils.replace(AppStringUtils.stringFilter(pybf.toString()), " ", "");
+		return "";
 	}
 
 	public static void main(String[] args) throws UnsupportedEncodingException {
-		String x = "工具  旅游！@#￥%……&*（）——++= 美 /     女“”   ";
-		System.out.println(cn2FirstSpell(x));
-		System.out.println(cn2Spell(x));
-		System.out.println(cn2Spell("/zhuanti/niandujingxu!:antafangyouxi /index"));
 	}
 
 }
