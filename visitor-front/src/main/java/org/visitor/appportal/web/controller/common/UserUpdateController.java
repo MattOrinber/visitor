9c6fd7bae5e265c03c6b64ea5a919e2b5cbd3ab8
wu meng <mengw@oupeng.com>
2014-12-21 11:59:54 +0800

    引入头文件改动
    Signed-off-by: wu meng <mengw@oupeng.com>

--- visitor-front/src/main/java/org/visitor/appportal/web/controller/common/UserUpdateController.java
+++ visitor-front/src/main/java/org/visitor/appportal/web/controller/common/UserUpdateController.java
@@ -8,7 +8,6 @@ import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
