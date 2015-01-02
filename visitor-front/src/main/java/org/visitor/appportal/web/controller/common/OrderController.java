32ee04874d742cff4ecee8c4c2542bd6d3a0513b
MattOrinber <mengw@oupeng.com>
2015-01-02 17:24:02 +0800

    extra price set 提交2
    Signed-off-by: MattOrinber <mengw@oupeng.com>

--- visitor-front/src/main/java/org/visitor/appportal/web/controller/common/OrderController.java
+++ visitor-front/src/main/java/org/visitor/appportal/web/controller/common/OrderController.java
@@ -6,7 +6,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -45,9 +44,6 @@ import org.visitor.appportal.web.utils.OrderInfo.ProductOrderStatusEnum;
 import org.visitor.appportal.web.utils.OrderInfo.ProductPayOrderStatusEnum;
 import org.visitor.appportal.web.utils.PaypalInfo;
 import org.visitor.appportal.web.utils.WebInfo;
-
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.TypeReference;
 
 @Controller
 @RequestMapping("/order/")
