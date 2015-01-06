ab14462f1588135a075220305c5853de35e152cd
MattOrinber <mengw@oupeng.com>
2015-01-06 12:07:07 +0800

    暂时不管facebook login
    Signed-off-by: MattOrinber <mengw@oupeng.com>

--- visitor-front/src/main/java/org/visitor/appportal/web/controller/common/BasicController.java
+++ visitor-front/src/main/java/org/visitor/appportal/web/controller/common/BasicController.java
@@ -313,9 +313,6 @@ public class BasicController {
 		try {
 			ifloggedIn = checkIfTheUserTokenLegal(model, request, response, b);
 			
-			List<TimeZone> listTZ = timezoneRedisService.getAllTimezones();
-			List<VisitorLanguage> listVL = visitorLanguageRedisService.getAllLanguages();
-			
 			List<String> homeTypeList = floopyThingRedisService.getFloopyValueList(FloopyUtils.HOME_TYPE_KEY);
 			List<String> roomTypeList = floopyThingRedisService.getFloopyValueList(FloopyUtils.ROOM_TYPE_KEY);
 			String singularAccomodates = floopyThingRedisService.getFloopyValueSingle(FloopyUtils.ACCOMODATES);
@@ -327,43 +324,12 @@ public class BasicController {
 			
 			List<String> currencyList = floopyThingRedisService.getFloopyValueList(FloopyUtils.CURRENCY_KEY);
 			
-			List<String> amenitiesMostCommon = floopyThingRedisService.getFloopyValueList(FloopyUtils.AMENITIES_MOST_COMMON);
-			List<String> amenitiesExtras = floopyThingRedisService.getFloopyValueList(FloopyUtils.AMENITIES_EXTRAS);
-			List<String> amenitiesSpecialFeatures = floopyThingRedisService.getFloopyValueList(FloopyUtils.AMENITIES_SPECIAL_FEATURES);
-			List<String> amenitiesHomeSafty = floopyThingRedisService.getFloopyValueList(FloopyUtils.AMENITIES_HOME_SAFETY);
-			
-			List<String> bedroomNumberList = floopyThingRedisService.getFloopyValueList(FloopyUtils.BEDROOM_NUMBER);
-			String bedsNumberListStr = floopyThingRedisService.getFloopyValueSingle(FloopyUtils.BED_NUMBER);
-			
-			if (StringUtils.isNotEmpty(bedsNumberListStr)) {
-				Integer bedsNumberListSize = Integer.valueOf(bedsNumberListStr);
-				List<String> bedsNumberList = floopyThingRedisService.getFloopySingularGeneratedList(bedsNumberListSize);
-				model.addAttribute("bedsNumberList", bedsNumberList);
-			}
-			
-			List<String> bathroomNumberList = floopyThingRedisService.getFloopyValueList(FloopyUtils.BATHROOM_NUMBER);
-			
-			List<String> checkinAfterList = floopyThingRedisService.getFloopyValueList(FloopyUtils.TERM_CHECKIN_AFTER);
-			List<String> checkoutBeforeList = floopyThingRedisService.getFloopyValueList(FloopyUtils.TERM_CHECKOUT_BEFORE);
 			List<String> cancellationPolicyList = floopyThingRedisService.getFloopyValueList(FloopyUtils.TERM_CANCELLATION_POLICY);
-			
-			model.addAttribute("timezones", listTZ);
-			model.addAttribute("visitorlanguages", listVL);
 			
 			model.addAttribute("homeTypeList", homeTypeList);
 			model.addAttribute("roomTypeList", roomTypeList);
 			model.addAttribute("currencyList", currencyList);
 			
-			model.addAttribute("amenitiesMostCommon", amenitiesMostCommon);
-			model.addAttribute("amenitiesExtras", amenitiesExtras);
-			model.addAttribute("amenitiesSpecialFeatures", amenitiesSpecialFeatures);
-			model.addAttribute("amenitiesHomeSafty", amenitiesHomeSafty);
-			
-			model.addAttribute("bedroomNumberList", bedroomNumberList);
-			model.addAttribute("bathroomNumberList", bathroomNumberList);
-			
-			model.addAttribute("checkinAfterList", checkinAfterList);
-			model.addAttribute("checkoutBeforeList", checkoutBeforeList);
 			model.addAttribute("cancellationPolicyList", cancellationPolicyList);
 			
 			String imgPathOrigin = MixAndMatchUtils.getSystemAwsPaypalConfig(MixAndMatchUtils.awsImgDomain) + MixAndMatchUtils.getSystemAwsPaypalConfig(MixAndMatchUtils.awsImgStatic);
